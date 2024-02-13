85c185bb5316408855eec3434336bc7631bb17a1
DefDervi <140266379+DefDervi@users.noreply.github.com>
2024-02-13 23:57:00 +0500

    Add files via upload

---
+++ star.js
@@ -0,0 +1,6 @@
+function random(x, y) {
+    return (x + (Math.random() * (y - x)));
+}
+for (var i = 0; i < 100; i++) {
+    document.body.innerHTML += '<div class="star" style="animation-duration: ' +random(10,50) + 's; top:' + random(0, 100) + '%; left: '+ random(0, 100) + '%; "></div>';
+}
